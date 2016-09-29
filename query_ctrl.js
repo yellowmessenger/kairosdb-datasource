8cc92fbc24b1110e6f1480527f0f666fbb71b56e
kishore <kishore@yellowmessenger.com>
2016-09-29 11:57:30 +0530

    Aggregation sampling issue

--- query_ctrl.js
+++ query_ctrl.js
@@ -319,6 +319,8 @@ define([
                     }
                 }
 
+                console.log(this.target);
+
                 if (!_.isEmpty(errors)) {
                     this.target.errors.horAggregator = errors;
                 }else{
